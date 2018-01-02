5f5e9cc043352fdc4134051a1bb5dc478f0f85df
wolftreeproperties <34863959+wolftreeproperties@users.noreply.github.com>
2018-01-02 12:39:51 -0800

    added drawer for mobile nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,20 +1,51 @@
-document.querySelector('#investments-nav-link').addEventListener('click', e => {
-	e.preventDefault();
-	switchPg(document.querySelector('.home-page'), 
-		document.querySelector('.investments-page'), 
-		() => document.querySelector('.navbar-brand').classList.add('show-logo')
-	);	
-});
+const investmentsLinks = 
+	document.querySelectorAll('.investments-nav-link'),
+drawer = document.querySelector('.drawer');
+
+for (var i = 0; i < investmentsLinks.length; ++i) {
+	investmentsLinks[i].addEventListener('click', e => {
+		e.preventDefault();
+		closeDrawer().then(() => 
+			switchPg(document.querySelector('.home-page'), 
+				document.querySelector('.investments-page'), 
+				() => {
+					document.querySelector('.navbar-brand').classList.add('show-logo');
+					document.querySelector('.page-wrapper').style.overflowY = 'auto';
+				}
+			)
+		);	
+	});
+}
 
 document.querySelector('.nav-logo').addEventListener('click', e => {
 	e.preventDefault();
 	switchPg(document.querySelector('.investments-page'), 
 		document.querySelector('.home-page'),
-		() => document.querySelector('.navbar-brand').classList.remove('show-logo')
+		() => {
+			document.querySelector('.navbar-brand').classList.remove('show-logo')
+			document.querySelector('.page-wrapper').style.overflowY = 'hidden';
+		}
 	);
 });
 
-let switchPg = (from, to, callback) => {
+const onAnimationComplete = resolve => {
+	drawer.removeEventListener('hidden.bs.modal', onAnimationComplete);
+	resolve();
+}
+
+const closeDrawer = () => {
+ 	return new Promise((resolve, reject) => {
+	  	if (!drawer.classList.contains('show'))
+	  		resolve();
+	  	else {
+	  		drawer.addEventListener('hidden.bs.modal',
+        		e => onAnimationComplete(resolve), false);
+	  		document.querySelector('.close').click();
+	  	}
+  });
+}
+
+const switchPg = (from, to, callback) => {
 	from.classList.remove('page-active');
 	from.classList.add('page-inactive');
 	to.classList.remove('page-inactive');
